723b183146f3ff65dc10b62b78f3c9cd09d2e157
Fereshteh Fayazfar <fereshtehfayazfar12gmail.com>
2020-11-05 19:41:23 +0330

    add logger

--- scrape/community.py
+++ scrape/community.py
@@ -1,6 +1,9 @@
+from logger import Logger
 from .request import send_request
 import re
 from db.models import Community
+
+logger = Logger.__call__().get_logger()
 
 """
 This function is for finding communities and also their sub communities.
@@ -16,9 +19,9 @@ def create_community(cookie, url):
     pattern_sub_community = re.compile(
         r"class=\"subforumicon subforum_minioff ajax_mark_read\" id=\"mark_read_[0-9]{1,}\"></div><a href=\"forumdisplay\.php\?fid=[0-9]{1,}\" title=\"\">([^</a>]*)")
     communities = re.findall(pattern_community, text)
-    print("comunities which is found is: ", communities)
+    logger.info(f"comunities which is found is: {communities} ")
     sub_communities = re.findall(pattern_sub_community, text)
-    print("sub comunities which is found is:", sub_communities)
+    logger.info(f"sub comunities which is found is: {sub_communities} ")
     if not communities:
         raise Exception("There is no cummunity!")
     for com in communities:
@@ -26,9 +29,9 @@ def create_community(cookie, url):
         try:
             new_community, status = Community.objects.get_or_create(title=com, url=url_of_community)
         except:
+            logger.error(f"There is more than one community with {com} title")
             # This exception should be handel due to business logic
-            pass
-        print(f"community is {new_community.title} ---> and the url is: {new_community.url}")
+        logger.info(f"community is {new_community.title} ---> and the url is: {new_community.url}")
         for sub in sub_communities:
             index_of_begin = text.find(com)
             sub_text = text[index_of_begin::]
@@ -38,8 +41,7 @@ def create_community(cookie, url):
                 try:
                     new, status = Community.objects.get_or_create(title=sub, parent=new_community, url=url_of_sub)
                 except:
-                    # This exception should be handel due to business logic
-                    pass
-                print(
-                    f"community is : {new_community.title} and the sub community is :{new.title} ----> the parent of sub community is : {new.parent.title}, and url is : {new.url}")
+                    logger.error(f"There is more than one sub community with {sub} title")
+
+                logger.info(f"community is : {new_community.title} and the sub community is :{new.title} ----> the parent of sub community is : {new.parent.title}, and url is : {new.url}")
     return "Add communities successfully"
--- scrape/request.py
+++ scrape/request.py
@@ -1,9 +1,16 @@
 import requests
 
+from logger import Logger
+
+logger = Logger.__call__().get_logger()
+
 
 def send_request(url,  cookies):
     try:
         request = requests.get(url, cookies=cookies)
+
     except requests.exceptions.HTTPError as err:
+        logger.error(f"There is error for get  response: {err}")
         raise SystemExit(err)
+    logger.info(f"getting response from the {url} was successfully.")
     return request
